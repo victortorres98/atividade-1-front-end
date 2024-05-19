858306d97f1ea2887a189a9e8c9c9a3c5d18c9a9
Victor R Torres <torres14victor@hotmail.com>
2024-05-19 14:27:44 -0300

    feat: subindo projeto

---
+++ webSite/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { MainComponent } from './componentes/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
